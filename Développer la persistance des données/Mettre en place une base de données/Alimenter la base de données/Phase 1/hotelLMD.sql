7acb7d01636b068eca83aa4456f862650909e27e
BaltoClem <dumasclementjulien@yahoo.fr>
2020-08-26 13:47:49 +0200

    Début de d'Alimenter la BDD

---
+++ Développer la persistance des données/Mettre en place une base de données/Alimenter la base de données/Phase 1/hotelLMD.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS hotelLMD;
+CREATE DATABASE hotelLMD;
+USE hotelLMD;
+
+CREATE TABLE Station (
+
+    stat_id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    stat_nom      VARCHAR(255)
+  
+);
+
+CREATE TABLE Hotel (
+
+    hotel_id        INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    hotel_nom       VARCHAR(255),
+    hotel_capa      INT,
+    hotel_categ     VARCHAR(255),
+    hotel_adresse   VARCHAR(255),
+    stat_id         INT NOT NULL,
+    FOREIGN KEY (stat_id) REFERENCES Station(stat_id)
+);
+
+CREATE TABLE Chambre (
+
+    chamb_id            INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    chamb_capa          VARCHAR(255),
+    chamb_deg_conf      VARCHAR(255),
+    chamb_expo          VARCHAR(255),
+    chamb_type          VARCHAR(255),
+    hotel_id            INT NOT NULL,
+    FOREIGN KEY (hotel_id) REFERENCES Hotel(hotel_id)
+);
+
+CREATE TABLE Client (
+
+    cli_id              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cli_nom             VARCHAR(255),
+    cli_prenom          VARCHAR(255),
+    cli_adresse         VARCHAR(255)      
+
+);
+
+CREATE TABLE Reservation (
+
+    reserv_date_debut           DATE,
+    reserv_date_fin             DATE,
+    reserv_date                 DATE,
+    reserv_montant_arr          INT,
+    reserv_prix_total           INT,
+    chamb_id                    INT NOT NULL,
+    cli_id                      INT NOT NULL,
+    PRIMARY KEY (chamb_id,cli_id),
+    FOREIGN KEY (chamb_id) REFERENCES Chambre(chamb_id),
+    FOREIGN KEY (cli_id) REFERENCES Client(cli_id)
+
+);
