a4d61279f40bc904761ed6cc2f9cb5be79c80174
BaltoClem <dumasclementjulien@yahoo.fr>
2020-08-27 10:29:13 +0200

    Alimenter la BDD ok

--- Développer la persistance des données/Mettre en place une base de données/Alimenter la base de données/Phase 2/papyrusLMD.sql
+++ Développer la persistance des données/Mettre en place une base de données/Alimenter la base de données/Phase 2/papyrusLMD.sql
@@ -1,15 +1,16 @@
-DROP DATABASE IF EXISTS papyrus3;
-CREATE DATABASE papyrus3;
-USE papyrus3;
+DROP DATABASE IF EXISTS papyrusLMD;
+CREATE DATABASE papyrusLMD;
+USE papyrusLMD;
 
 CREATE TABLE produit (
 
 CODART      CHAR(4) NOT NULL PRIMARY KEY,
 LIBART      VARCHAR(30) NOT NULL,
+UNIMES      CHAR(5) NOT NULL,
 STKALE      INT(10) NOT NULL,
 STKPHY      INT(10) NOT NULL,
-QTEANN      INT(10) NOT NULL,
-UNIMES      CHAR(5) NOT NULL
+QTEANN      INT(10) NOT NULL
+
 
 );
 
@@ -27,6 +28,8 @@ SATISF      TINYINT(2)
 
 CREATE TABLE vente (
 
+NUMFOU      VARCHAR(25) NOT NULL,
+CODART      CHAR(4) NOT NULL,
 DELLIV      SMALLINT(5) NOT NULL,
 QTE1        INT(10) NOT NULL,
 PRIX1       VARCHAR(50) NOT NULL,
@@ -34,8 +37,6 @@ QTE2        INT(10),
 PRIX2       VARCHAR(50),
 QTE3        INT(10),
 PRIX3       VARCHAR(50),
-NUMFOU      VARCHAR(25) NOT NULL,
-CODART      CHAR(4) NOT NULL,
 PRIMARY KEY (NUMFOU, CODART),
 FOREIGN KEY (NUMFOU) REFERENCES fournis(NUMFOU),
 FOREIGN KEY (CODART) REFERENCES produit(CODART)
@@ -67,3 +68,33 @@ FOREIGN KEY (NUMCOM) REFERENCES entcom(NUMCOM),
 FOREIGN KEY (CODART) REFERENCES produit(CODART)
 
 );
+
+/* 
+
+L'ordre à adopter pour alimenter la BDD pourrait être :
+
+produit, fournis, vente, ligcom et entcom
+
+Cet ordre est idéal puisque les deux premières tables n'ont pas de clés étrangères selon
+le MCD de Papyrus, du coup aucun conflits liés à ces clés étrangères ne peut
+arriver
+
+*/
+
+INSERT INTO fournis (NUMFOU, NOMFOU, RUEFOU, POSFOU, VILFOU, CONFOU, SATISF)
+VALUES  ('00120', 'GROBRIGAN', '20 rue du papier', '92200', 'Papercity', 'Georges', 08),
+        ('00540', 'ECLIPSE', '53 rue laisse flotter les rubans', '78250', 'Bugbugville', 'Nestor', 07),
+        ('08700', 'MEDICIS', '120 rue des plantes', '75014', 'Paris', 'Lison', NULL),
+        ('09120', 'DISCOBOL', '11 rue des sports', '85100', 'La Roche sur Yon', 'Hercule', 08),
+        ('09150', 'DEPANPAP', '26 avenue des locomotives', '59987', 'Coroncountry', 'Pollux', 05),
+        ('09180', 'HURRYTAPE ', '68 boulevard des octets', '04044', 'Dumpville', 'Track', NULL)
+        
+
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\80010-91-10\\Documents\\Travaux_CDA\\vente.csv'
+INTO TABLE vente
+FIELDS TERMINATED BY ';' 
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(NUMFOU,CODART,DELLIV,QTE1,PRIX1,QTE2,PRIX2,QTE3,PRIX3);
+
