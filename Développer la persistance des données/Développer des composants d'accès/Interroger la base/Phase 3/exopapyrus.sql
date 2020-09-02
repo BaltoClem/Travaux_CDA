0811cc3f7bc27cc5d07d1e4f035c8345082cd4ee
BaltoClem <dumasclementjulien@yahoo.fr>
2020-09-02 16:26:15 +0200

    Début Phase 3 Interroger la base

---
+++ Développer la persistance des données/Développer des composants d'accès/Interroger la base/Phase 3/exopapyrus.sql
@@ -0,0 +1,8 @@
+/* --------- LES BESOINS D’AFFICHAGE -----------------*/
+
+/* Quelles sont les commandes du fournisseur 09120 ? */
+
+SELECT numcom AS "Numéro de commande"
+FROM entcom
+WHERE numfou = 9120;
+
