ce188031addef60e273bb7ad6e2deee8d2601e15
BaltoClem <dumasclementjulien@yahoo.fr>
2020-09-03 15:58:42 +0200

    Exercice 1 Créer des vues ok

--- Développer la persistance des données/Développer des composants d'accès/Créer des vues/Exercice 1/hotel.sql
+++ Développer la persistance des données/Développer des composants d'accès/Créer des vues/Exercice 1/hotel.sql
@@ -10,4 +10,50 @@ JOIN station
 ON hot_sta_id = sta_id
 ORDER BY hot_nom;
 
-SELECT * FROM v_hotstat
+SELECT * FROM v_hotstat;
+
+/* Afficher la liste des chambres et leur hôtel */
+
+CREATE VIEW v_chahot
+AS
+SELECT cha_numero AS "Numéro de chambre", hot_nom AS "Nom d'hôtel"
+FROM chambre
+JOIN hotel
+ON cha_hot_id = hot_id
+ORDER BY cha_numero;
+
+SELECT * FROM v_chahot;
+
+/* Afficher la liste des réservations avec le nom des clients */
+
+CREATE VIEW v_rescli
+AS
+SELECT res_id AS "Numéro de réservation", cli_nom AS "Nom du client"
+FROM reservation
+JOIN client
+ON res_cli_id = cli_id
+ORDER BY res_id;
+
+SELECT * FROM v_rescli;
+
+/* Afficher la liste des chambres avec le nom de l’hôtel et le nom de la station */
+
+CREATE VIEW v_chahotres
+AS
+SELECT cha_numero AS "Numéro de chambre", hot_nom AS "Numéro de l'hôtel", sta_nom AS "Nom de la station"
+FROM chambre, hotel, station
+WHERE cha_hot_id = hot_id AND hot_sta_id = sta_id
+ORDER BY cha_numero;
+
+SELECT * FROM v_chahotres;
+
+/* Afficher les réservations avec le nom du client et le nom de l’hôtel */
+
+CREATE VIEW v_resclihot
+AS
+SELECT res_id AS "Numéro de réservation", cli_nom AS "Nom du client", hot_nom AS "Nom de l'hôtel"
+FROM reservation, client, chambre, hotel
+WHERE res_cli_id = cli_id AND res_cha_id = cha_id AND cha_hot_id = hot_id
+ORDER BY res_id;
+
+SELECT * FROM v_resclihot;
