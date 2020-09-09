b57726f72d3f28aa74d89273cf364237ca9a590f
BaltoClem <dumasclementjulien@yahoo.fr>
2020-09-09 14:13:04 +0200

    Exercice 1 Evaluation Composants accès ok

--- Développer la persistance des données/Développer des composants d'accès/Evaluation/1 - Requêtes d'interrogation/requetes.sql
+++ Développer la persistance des données/Développer des composants d'accès/Evaluation/1 - Requêtes d'interrogation/requetes.sql
@@ -39,3 +39,43 @@ WHERE `order details`.OrderID = orders.OrderID AND orders.CustomerID = customers
 GROUP BY CompanyName, Country
 HAVING SUM(UnitPrice*Quantity) > 30000
 ORDER BY SUM(UnitPrice*Quantity) DESC;
+
+/* 6 – Liste des pays dont les clients ont passé commande de produits 
+fournis par « Exotic Liquids »  */
+
+SELECT DISTINCT customers.Country AS "Pays"
+FROM suppliers, products, `order details`, orders, customers
+WHERE suppliers.SupplierID = products.SupplierID
+AND products.ProductID = `order details`.ProductID
+AND `order details`.OrderID = orders.OrderID
+AND orders.CustomerID = customers.CustomerID
+AND suppliers.CompanyName = "Exotic Liquids"
+ORDER BY customers.Country ASC;
+
+/* 7 – Montant des ventes de 1997  */
+
+SELECT SUM(UnitPrice*Quantity) AS "Montant des ventes de 1997"
+FROM orders, `order details`
+WHERE `order details`.OrderID = orders.OrderID
+AND YEAR(orders.OrderDate) = 1997;
+
+/* 8 – Montant des ventes de 1997 mois par mois  */
+
+SELECT MONTH(orders.OrderDate) AS "Mois 1997", SUM(UnitPrice*Quantity) AS "Montant des ventes de 1997"
+FROM orders, `order details`
+WHERE `order details`.OrderID = orders.OrderID
+AND YEAR(orders.OrderDate) = 1997
+GROUP BY MONTH(orders.OrderDate)
+ORDER BY MONTH(orders.OrderDate) ASC;
+
+/* 9 – Depuis quelle date le client « Du monde entier » n’a plus commandé ? */
+
+SELECT MAX(orders.OrderDate) AS "Date de dernière commande"
+FROM orders, customers
+WHERE orders.CustomerID = customers.CustomerID
+AND CompanyName = "Du monde entier";
+
+/* 10 – Quel est le délai moyen de livraison en jours ? */
+
+SELECT ROUND(AVG(DATEDIFF(ShippedDate, OrderDate))) AS "Délai moyen de livraison en jours"
+FROM orders;
