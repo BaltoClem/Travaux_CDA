67685be31b42c7ba0f3a1a0ce5a58e052d182be5
BaltoClem <dumasclementjulien@yahoo.fr>
2020-09-09 11:19:32 +0200

    Phase 2 Triggers ok

--- Développer la persistance des données/Développer des composants d'accès/Les déclencheurs/Phase 2/phase2.sql
+++ Développer la persistance des données/Développer des composants d'accès/Les déclencheurs/Phase 2/phase2.sql
@@ -1,4 +1,6 @@
-/* ---- Création d'un déclencheur AFTER UPDATE ---- */
+/* ---- MySql : les déclencheurs, Papyrus ---- */
+
+/* Création d'un déclencheur AFTER UPDATE */
 
 /* Créer une table ARTICLES_A_COMMANDER */
 
@@ -10,3 +12,37 @@ qte         INT,
 FOREIGN KEY(codart) REFERENCES produit(codart)
 
 );
+
+/* Créer un déclencheur UPDATE sur la table produit */
+
+
+DELIMITER //
+
+CREATE TRIGGER prod_maj AFTER UPDATE ON produit
+    FOR EACH ROW
+    BEGIN
+
+    DECLARE stockale INT;
+    DECLARE stockphy INT;
+    DECLARE codarticle CHAR(4);
+    DECLARE datecom DATETIME;
+    DECLARE qteacom INT;
+
+    SET codarticle = NEW.codart;
+    SET stockale = (SELECT stkale FROM produit WHERE codart = codarticle);
+    SET stockphy = (SELECT stkphy FROM produit WHERE codart = codarticle);
+    SET qteacom = (SELECT qte FROM articles_a_commander WHERE codart = codarticle);
+    SET datecom = (SELECT datecomm FROM articles_a_commander WHERE codart = codarticle);
+        IF stockphy < stockale AND qteacom IS NULL THEN
+        INSERT INTO articles_a_commander(codart, datecomm, qte)
+        VALUES ( NEW.codart, NOW(), stockale - stockphy);
+            ELSEIF qteacom IS NOT NULL THEN
+            UPDATE articles_a_commander 
+            SET qte=((stockale - stockphy) - qteacom)
+            WHERE codart = codarticle;
+        END IF;
+    END;
+
+//
+
+DELIMITER ;
