345d971478d19c327471a5f6805b975ccc2a4407
BaltoClem <dumasclementjulien@yahoo.fr>
2020-09-10 10:40:10 +0200

    Composants accès Fil Rouge en cours

---
+++ Projet Fil Rouge/Module 1 - Base de données/Développer des composants d'accès/requeteform.sql
@@ -0,0 +1,10 @@
+/*-------- Formaliser des requêtes à l'aide du langage SQL -----------------*/
+
+/* 1 - Pour chacune des interrogations demandées pour la réalisation 
+du tableau de bord (voir cahier des charges), 
+créez un script contenant la ou les requêtes nécessaires. */
+
+
+/* 2 - Exportez les tables principales (entité) 
+vers des tableaux d’un tableur de votre choix. */
+
