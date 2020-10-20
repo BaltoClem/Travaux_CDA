e39b9652e465fcaa14ed75900007cee6fbfab31e
BaltoClem <dumasclementjulien@yahoo.fr>
2020-10-20 14:53:13 +0200

    Formulaire ajout Velvet Records ok

--- Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/add_disc.php
+++ Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/add_disc.php
@@ -1,6 +1,11 @@
 <?php
 
 include("index.php");
+include("process.php");
+
+$requete_add = $db->query("SELECT * FROM artist");
+$tableau_add = $requete_add->fetchAll(PDO::FETCH_OBJ);
+$requete_add->closeCursor();
 
 ?>
 
@@ -21,33 +26,47 @@ include("index.php");
         <div class="row justify-content-center">
             <h1>Ajout d'un vinyle</h1>
         </div>
-        <form>
+        <form enctype="multipart/form-data" name="add_form" method="post" action="add_process.php">
             <div class="form-group">
                 <label for="inputTitle">Title</label>
-                <input type="text" class="form-control" id="inputTitle" placeholder="Ajouter un titre">
+                    <input type="text" class="form-control" id="inputTitle" placeholder="Ajouter un titre" name="title">
             </div>
             <div class="form-group">
                 <label for="inputArtist">Artist</label>
-                <input type="text" class="form-control" id="inputArtist" placeholder="Ajouter l'artiste">
+                    <select class="form-control" id="inputArtist" name="artist">
+                        <option selected disabled> -- Choisir un artiste --</option>
+                        <?php foreach($tableau_add as $artist)
+                        {
+                        ?>
+                            <option value="<?= $artist->artist_id ?>"><?= $artist->artist_name ?></option>
+                            <?php
+                        }
+                        ?>
+                    </select>
             </div>
             <div class="form-group">
                 <label for="inputTitle">Year</label>
-                <input type="text" class="form-control" id="inputYear" placeholder="Ajouter l'année de parution">
+                    <input type="text" class="form-control" id="inputYear" placeholder="Ajouter l'année de parution" name="year">
             </div>
             <div class="form-group">
                 <label for="inputGenre">Genre</label>
-                <input type="text" class="form-control" id="inputGenre" placeholder="Ajouter le genre du disque">
+                    <input type="text" class="form-control" id="inputGenre" placeholder="Ajouter le genre du disque" name="genre">
             </div>
             <div class="form-group">
                 <label for="inputLabel">Label</label>
-                <input type="text" class="form-control" id="inputLabel" placeholder="Ajouter le label">
+                    <input type="text" class="form-control" id="inputLabel" placeholder="Ajouter le label" name="label">
             </div>
             <div class="form-group">
                 <label for="inputPrice">Price</label>
-                <input type="text" class="form-control" id="inputPrice" placeholder="Ajouter le prix">
+                    <input type="text" class="form-control" id="inputPrice" placeholder="Ajouter le prix" name="price">
+            </div>
+            <div class="form-group">
+                <label for="inputFile">Picture</label>
+                    <input type="file" class="form-control-file" id="inputFile" name="userfile">
             </div>
             <div class="row justify-content-center">
-            <button type="submit" class="btn btn-success">Valider l'ajout</button>
+                <button type="submit" class="btn btn-success" name="valid_add">Valider l'ajout</button>
+                <a href="list_disc.php"><button type="button" class="btn btn-secondary ml-4">Retour</button></a>
             </div>
             <br>
         </form>
