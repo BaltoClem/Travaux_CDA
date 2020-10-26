e3fc892e94edbbaecc77df184192238ce09f9e1b
BaltoClem <dumasclementjulien@yahoo.fr>
2020-10-26 11:44:28 +0100

    Cards Responsive ok + Navbar ok + Redimensionnement images ok

--- Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/index.php
+++ Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/index.php
@@ -6,7 +6,8 @@ include("header.php");
 $requete_card = $db->query("SELECT * 
                                     FROM disc 
                                     JOIN artist 
-                                    ON disc.artist_id = artist.artist_id");
+                                    ON disc.artist_id = artist.artist_id
+                                    ORDER BY disc_id");
 $tableau_card = $requete_card->fetchAll(PDO::FETCH_OBJ);
 $requete_card->closeCursor();
 
@@ -15,67 +16,67 @@ $result_nb_disc = $requete_nb_disc->fetch(PDO::FETCH_OBJ);
 $requete_nb_disc->closeCursor();
 $nb_disc = $result_nb_disc->nb_disc;
 
-$requete_last_disc = $db->query("SELECT disc_title AS nom_disc, disc_id, disc_picture AS img_album FROM disc ORDER BY disc_id DESC LIMIT 5");
+$requete_last_disc = $db->query("SELECT disc_title AS nom_disc, disc_picture AS img_album FROM disc ORDER BY disc_id DESC LIMIT 5");
 $result_last_disc = $requete_last_disc->fetchAll(PDO::FETCH_OBJ);
 $requete_last_disc->closeCursor();
 
 ?>
-    <div id="list_disc">
-    <div class="container">
-        <?php include("navbar.php");?>
-
-<header>
-            <div class="video-section-list">
-                <div class="row justify-content-left">
-                <p class="h1">Liste des disques&nbsp(<?php echo $nb_disc ?>)</p>
+        <div class="container">
+            <body>
+                <header>
+                            <div class="video-section-list">
+                                <div class="row justify-content-left">
+                                <p class="h1">Liste des disques&nbsp(<?php echo $nb_disc ?>)</p>
+                                </div>
+                                    <video id="video-elem-list" preload autoplay loop muted>
+                                        <source src="./assets/video/Turntable.mp4" type="video/mp4">
+                                    </video>
+                            </div>
+                </header>
+                <br>
+                <div class="row justify-content-center">
+                    <a href="add_form.php"><button type="button" class="btn btn-success">Ajouter un disque</button></a>
                 </div>
-                    <video id="video-elem-list" preload autoplay loop muted>
-                        <source src="./assets/video/Turntable.mp4" type="video/mp4">
-                    </video>
-            </div>
-</header>
-
-    <body>
-            <br>
-            <div class="row justify-content-center">
-            <a href="add_form.php"><button type="button" class="btn btn-success">Ajouter un disque</button></a>
-            </div>
-            <br>
-            <div class="row justify-content-center">
-            <p>Récemment ajoutés&nbsp:&nbsp</p>
-                <ul class="list-group striped-list">
-                    <?php foreach($result_last_disc as $last_disc){?>
-                        <li class="list-group-item">
-                            <img src="assets/pictures/<?= $last_disc -> img_album?>" alt="album" class="thumbnail">&nbsp<?= $last_disc -> nom_disc?> &nbsp
-                        </li>
-                    <?php } ?>
-                </ul>
-            </div>
-
-        <div class="row d-flex justify-content-center mt-5 col-lg-12">
-            <?php
-                foreach($tableau_card as $disc)
-                {
-            ?>
-            <div class="card text-center mb-2 ml-2">
-                <div class="card-header">
-                    <img src="./assets/pictures/<?= $disc->disc_picture?>" class="card-img-top" alt="Photo album">
+                <br>
+                <div class="row justify-content-center h6">
+                    <p class="mt-4">Récemment ajoutés&nbsp:&nbsp</p>
+                        <ul class="list-group list-group-horizontal">
+                            <?php foreach($result_last_disc as $last_disc){?>
+                                <li class="list-group-item">
+                                    <div class="row">
+                                        <img src="assets/pictures/<?= $last_disc -> img_album?>" alt="album" class="thumbnail">
+                                            <p class="mt-3">&nbsp<?= $last_disc -> nom_disc?> &nbsp</p>
+                                    </div>
+                                </li>
+                            <?php } ?>
+                        </ul>
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title"><?= $disc->disc_title ?></h5>
-                        <h6 class="card-title"><?= $disc->artist_name ?></h6>
-                            <p class="card-text text-truncate">Label&nbsp:&nbsp<?= $disc->disc_label ?></p>
-                            <p class="card-text text-truncate">Year&nbsp:&nbsp<?= $disc->disc_year ?></p>
-                            <p class="card-text text-truncate">Genre&nbsp:&nbsp<?= $disc->disc_genre ?></p>
-                            <a href="details.php?disc_id=<?= $disc->disc_id ?>" class="btn btn-info">Détails</a>
-                </div>
-            </div>
-            <?php
-            } 
-            ?>
+
+                    <div class="row d-flex justify-content-center mt-5">
+                        <?php
+                            foreach($tableau_card as $disc)
+                            {
+                        ?>
+                        <div class="card text-center mb-2 ml-2">
+                            <div class="card-header">
+                                <img src="./assets/pictures/<?= $disc->disc_picture?>" class="card-img-top" alt="Photo album">
+                            </div>
+                            <div class="card-body">
+                                <p class="card-title h5"><?= $disc->disc_title ?></p>
+                                    <p class="card-title h6"><?= $disc->artist_name ?></p>
+                                        <p class="card-text text-truncate">Label&nbsp:&nbsp<?= $disc->disc_label ?></p>
+                                            <p class="card-text text-truncate">Year&nbsp:&nbsp<?= $disc->disc_year ?></p>
+                                                <p class="card-text text-truncate">Genre&nbsp:&nbsp<?= $disc->disc_genre ?></p>
+                                                    <a href="details.php?disc_id=<?= $disc->disc_id ?>" class="btn btn-warning">Détails</a>
+                            </div>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+
         </div>
-    </div>
-</div>
+
 <?php
 include("footer.php");
 ?>
--- Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/navbar.php
+++ Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/navbar.php
@@ -1,25 +1,30 @@
-<?php include("header.php") ?>
-
 <nav class="navbar navbar-expand-lg navbar-light">
   <a class="navbar-brand" href="#">Velvet_Records</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="add_form.php">Ajouter un disque</a>
       </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Espace Client</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">S'inscrire</a>
+                <a class="dropdown-item" href="#">Connexion</a>
+            </div>
+        </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
         <label>
-            <input class="form-control mr-sm-2" type="search" placeholder="Search">
+            <input class="form-control mr-sm-2" type="search" placeholder="Recherche...">
         </label>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <button class="btn btn-outline-dark my-sm-0" id="btn_search" type="submit">Rechercher</button>
     </form>
   </div>
 </nav>
