d7cb0a6a3f663482bd07c2391ac1aefc1be7cfb1
BaltoClem <dumasclementjulien@yahoo.fr>
2020-10-27 11:49:10 +0100

    Sécurisation mot de passe PHP

--- Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/login_script.php
+++ Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/login_script.php
@@ -1,15 +1,43 @@
 <?php
 
+session_start();
 include("process.php");
 
 $email = $_POST['email'];
-$password = $_POST['password'];
 
-$pdoStat= $db->prepare("SELECT * FROM users WHERE user_email =".$email);
+$pdoStat= $db->prepare("SELECT * FROM users WHERE user_email = :user_email");
+$pdoStat->bindValue(':user_email', $email, PDO::PARAM_STR);
+$pdoStat -> execute();
 
-$password_verify = password_verify($password, $user_detail->user_password);
+$user = $pdoStat ->fetch(PDO::FETCH_OBJ);
 
-if($password === $password_verify){
+if(password_verify($_POST['password'], $user->user_password)){
     $_SESSION["auth"] = "ok";
-    echo "Utilisateur trouvé";
+    echo "Utilisateur trouvé !";
+    var_dump($_SESSION["auth"]);
+    echo"- session ID : ".session_id();
+    header("Refresh:5;url=index.php");
 }
+else{
+    echo "Erreur : l'identifiant ou le mot de passe sont incorrects !";
+
+        if(!isset($_SESSION['essai'])){
+            $_SESSION['essai'] = 1;
+        }
+        else{
+            $_SESSION['essai'] ++;
+            var_dump($_SESSION['essai']);
+                if($_SESSION['essai'] === 4){
+                    echo "Nombre d'essais trop important";
+                }
+        }
+
+}
+
+//unset($_SESSION["auth"]);
+//if (ini_get("session.use_cookies"))
+//{
+//    setcookie(session_name(), '', time()-42000);
+//}
+//session_destroy();
+//echo "Erreur";
--- Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/registration_script.php
+++ Développer des composants d'interface/Développer la partie back-end/PHP/Connexion aux bases de données avec PDO/Velvet_records/registration_script.php
@@ -2,17 +2,38 @@
 
 include("process.php");
 
-$pdoStat = $db->prepare("INSERT INTO users(user_email, user_password)
+//Vérification d'un email déjà existant
+
+$email = $_POST['email'];
+$stmt = $db->prepare("SELECT * FROM users WHERE user_email=?");
+$stmt->execute([$email]);
+$user = $stmt->fetch();
+if ($user) {
+    echo "L'adresse email existe déjà";
+    exit();
+}
+
+//Comparaison de mots de passe et vérification de robustesse
+
+if($_POST['password'] != $_POST['confirmPassword']){
+    echo "Mots de passe différents";
+    exit();
+}
+elseif(!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/", ($_POST["password"]))){
+    echo "Mot de passe trop faible";
+    exit();
+}
+
+//Insertion du nouvel utilisateur
+
+$new_user = $db->prepare("INSERT INTO users(user_email, user_password)
                                 VALUES(:user_email, :user_password)");
 
 $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-$pdoStat->bindValue(':user_email', $_POST['email'], PDO::PARAM_STR);
-$pdoStat->bindValue(':user_password', $password_hash, PDO::PARAM_STR);
+$new_user->bindValue(':user_email', $_POST['email'], PDO::PARAM_STR);
+$new_user->bindValue(':user_password', $password_hash, PDO::PARAM_STR);
 
-if($_POST['password'] != $_POST['confirmPassword']){
-    exit();
-}
-$pdoStat->execute();
+$new_user->execute();
 
 header("Location:index.php");
