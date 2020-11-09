4a46e88fc33d2350566c7d63c41ea7a722a3637c
BaltoClem <dumasclementjulien@yahoo.fr>
2020-11-09 11:57:22 +0100

    Création vue Home ok + vue Profil en cours

---
+++ Développer des composants d'interface/Développer la partie back-end/MVC/symfony/symfony_cda/src/Controller/ProfilController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/profil")
+ * @method render(string $string, array $array)
+ */
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/detail", name="profil")
+     */
+    public function detail() :Response
+    {
+        // affichage de la page d'accueil
+        return $this->render('profil/detail.html.twig');
+    }
+}
