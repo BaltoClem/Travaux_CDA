3b7f4b8c1cd17d75ddcb80a6a88a8d1e3444186d
BaltoClem <dumasclementjulien@yahoo.fr>
2020-11-16 14:34:44 +0100

    Fin Voters Symfony

--- Developper des composants d'interface/Developper la partie back-end/MVC/symfony/exempleDoctrine/src/Controller/UserController.php
+++ Developper des composants d'interface/Developper la partie back-end/MVC/symfony/exempleDoctrine/src/Controller/UserController.php
@@ -66,6 +66,8 @@ class UserController extends AbstractController
 
     /**
      * @Route("/{id}", name="user_show", methods={"GET"})
+     * @param User $user
+     * @return Response
      */
     public function show(User $user): Response
     {
