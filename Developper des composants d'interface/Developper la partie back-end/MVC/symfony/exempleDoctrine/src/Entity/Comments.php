4a143ca6a5dd99791d22aff6e3aefb8c71baea69
BaltoClem <dumasclementjulien@yahoo.fr>
2020-11-17 13:43:06 +0100

    Entity Comments terminée + Migrations

--- Developper des composants d'interface/Developper la partie back-end/MVC/symfony/exempleDoctrine/src/Entity/Comments.php
+++ Developper des composants d'interface/Developper la partie back-end/MVC/symfony/exempleDoctrine/src/Entity/Comments.php
@@ -17,8 +17,59 @@ class Comments
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updateDate;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUpdateDate(): ?\DateTimeInterface
+    {
+        return $this->updateDate;
+    }
+
+    public function setUpdateDate(?\DateTimeInterface $updateDate): self
+    {
+        $this->updateDate = $updateDate;
+
+        return $this;
     }
 }
