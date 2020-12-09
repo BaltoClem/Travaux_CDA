df4bfb521bbc041be440b311488010b0c3d75118
BaltoClem <dumasclementjulien@yahoo.fr>
2020-12-09 09:50:48 +0100

    AJAX TheMovieDB terminé

--- Developper des composants d'interface/Developper la partie back-end/Ajax - JQuery/Phase 5 - Utilisation de l'API TheMovieDB/script.js
+++ Developper des composants d'interface/Developper la partie back-end/Ajax - JQuery/Phase 5 - Utilisation de l'API TheMovieDB/script.js
@@ -1,13 +1,12 @@
-
 $('#submit').click(function() {
 
     let search = $("#search").val();
-    console.log(search);
-    $.getJSON("http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query=") + search, function (data){
+    $.getJSON("http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query="+search, function (data){
         var items = [];
         $.each(data.results, function(key, val) {
-            items.push(key + ' : ' + val.title + '<br />');
+            items.push('<br />' + key + ' : ' + val.original_title + '<br />' + val.release_date + '<br />' + 
+            '<img src="http://image.tmdb.org/t/p/w185' + val.poster_path + '">');
         });
     $('#result').html(items.join());
-    }
+});
 });
