3c669c140e12655d57bc2c9c47afac2e75237d49
BaltoClem <dumasclementjulien@yahoo.fr>
2020-12-11 11:07:24 +0100

    Exercice Interface Vue.JS ok

---
+++ Developper des composants d'interface/Developper la partie back-end/Vue.js/Cours ncode/Exercices/script4.js
@@ -0,0 +1,12 @@
+/**
+ * Interface
+ */
+// initialisation de l'objet Vue
+var app = new Vue({
+    // identifie la partie html cibléé
+    el: '#interface',
+    data: {
+        showSquare1 : true,
+        showSquare2 : false
+    },
+});
