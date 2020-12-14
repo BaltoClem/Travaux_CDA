ed2c33ebe75318c9344f9b5d7631bd1389579ff6
BaltoClem <dumasclementjulien@yahoo.fr>
2020-12-14 14:01:15 +0100

    Premier programme Java

---
+++ Developper des composants d'interface/Developper une interface de type desktop/Developper avec Java/1 - Le premier programme Java/helloJava/Hello.java
@@ -0,0 +1,8 @@
+//On déclare ici le nom du dossier dont fait partie le fichier
+package helloJava;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("premier package");
+    }
+}
