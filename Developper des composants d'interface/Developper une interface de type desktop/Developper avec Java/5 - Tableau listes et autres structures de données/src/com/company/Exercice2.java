20c5e2397a608de2068e4644a8b5813a9ec262c9
BaltoClem <dumasclementjulien@yahoo.fr>
2020-12-16 13:53:48 +0100

    Exercice 2 Tableaux JAVA

--- Developper des composants d'interface/Developper une interface de type desktop/Developper avec Java/5 - Tableau listes et autres structures de données/src/com/company/Exercice2.java
+++ Developper des composants d'interface/Developper une interface de type desktop/Developper avec Java/5 - Tableau listes et autres structures de données/src/com/company/Exercice2.java
@@ -1,10 +1,44 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercice2 {
     public static void main(String[] args) {
-        System.out.println("****  Exercice 2 (15/12/2020) ****");
+        System.out.println("****  Exercice 2 (16/12/2020) ****");
 
-        Scanner scanner_1 = new Scanner(System.in);
+        Scanner scanner_2 = new Scanner(System.in);
+
+        ArrayList<String> tab = new ArrayList<>();
+
+        tab.add("Audrey");
+        tab.add("Aurélien");
+        tab.add("Flavien");
+        tab.add("Jérémy");
+        tab.add("Laurent");
+        tab.add("Melik");
+        tab.add("Nouara");
+        tab.add("Salem");
+        tab.add("Samuel");
+        tab.add("Stéphane");
+        tab.add("Hervé");
+        tab.add("Fodé");
+        tab.add("Marion");
+        tab.add("Manu");
+        tab.add("Germain");
+        tab.add("François");
+        tab.add("Pauline");
+        tab.add("Margot");
+        tab.add("Clément");
+        tab.add("Caroline");
+
+        System.out.println("Saisissez un prénom :");
+        String prenom = scanner_2.next();
+
+        for (String s : tab) {
+            if(prenom.equals(s)){
+                tab.set(tab.indexOf(prenom), " ");
+                System.out.println(tab);
+            }
+            }
+        }
     }
-}
