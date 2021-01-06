a41f02b791711bcab214e497b86a8267c34957fd
BaltoClem <dumasclementjulien@yahoo.fr>
2021-01-06 11:59:45 +0100

    JavaFX Exercice 2 objets graphiques en cours

--- Developper des composants d'interface/Developper une interface de type desktop/Composant graphique JavaFX/Phase 3 - Les objets graphiques/Exercice 2/src/main/java/afpa/clement/org/CasesACocher.java
+++ Developper des composants d'interface/Developper une interface de type desktop/Composant graphique JavaFX/Phase 3 - Les objets graphiques/Exercice 2/src/main/java/afpa/clement/org/CasesACocher.java
@@ -1,7 +1,12 @@
 package afpa.clement.org;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CasesACocher extends Application {
 
@@ -10,7 +15,10 @@ public class CasesACocher extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+            Parent root = FXMLLoader.load(getClass().getResource("/afpa/clement/org/gui/Case.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
     }
 }
--- Developper des composants d'interface/Developper une interface de type desktop/Composant graphique JavaFX/Phase 3 - Les objets graphiques/Exercice 2/src/main/java/afpa/clement/org/gui/CaseController.java
+++ Developper des composants d'interface/Developper une interface de type desktop/Composant graphique JavaFX/Phase 3 - Les objets graphiques/Exercice 2/src/main/java/afpa/clement/org/gui/CaseController.java
@@ -1,4 +1,51 @@
 package afpa.clement.org.gui;
 
+import javafx.scene.Group;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class CaseController {
+    public TextField textfield1;
+    public TitledPane choix_pane;
+    public CheckBox couleur_fond_choice;
+    public CheckBox couleur_texte_choice;
+    public CheckBox casse_choice;
+    public Label label1;
+    public TitledPane fond_pane;
+    public RadioButton fond_rouge_button;
+    public RadioButton fond_bleu_button;
+    public RadioButton fond_vert_button;
+    public TitledPane texte_pane;
+    public RadioButton texte_rouge_button;
+    public RadioButton texte_noir_button;
+    public RadioButton texte_blanc_button;
+    public TitledPane casse_pane;
+    public RadioButton casse_min_button;
+    public RadioButton casse_maj_button;
+
+    public void key_released1(KeyEvent keyEvent) {
+        label1.setText(textfield1.getText());
+    }
+// if() pour si la case couleur de fond est cochée
+    public void mouse_clicked_red(MouseEvent mouseEvent) {
+        fond_rouge_button.setSelected(true);
+        fond_bleu_button.setSelected(false);
+        fond_vert_button.setSelected(false);
+        label1.setStyle("-fx-background-color: red;");
+    }
+
+    public void mouse_clicked_blue(MouseEvent mouseEvent) {
+        fond_rouge_button.setSelected(false);
+        fond_bleu_button.setSelected(true);
+        fond_vert_button.setSelected(false);
+        label1.setStyle("-fx-background-color: blue;");
+    }
+
+    public void mouse_clicked_green(MouseEvent mouseEvent) {
+        fond_rouge_button.setSelected(false);
+        fond_bleu_button.setSelected(false);
+        fond_vert_button.setSelected(true);
+        label1.setStyle("-fx-background-color: green;");
+    }
 }
