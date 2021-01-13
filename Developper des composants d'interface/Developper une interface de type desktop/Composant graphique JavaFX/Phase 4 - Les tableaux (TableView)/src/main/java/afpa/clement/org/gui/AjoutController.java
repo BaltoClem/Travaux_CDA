153e624532d2cb858835c15e8f7771a4c9cb6170
BaltoClem <dumasclementjulien@yahoo.fr>
2021-01-13 09:37:31 +0100

    JavaFX TableView terminé

--- Developper des composants d'interface/Developper une interface de type desktop/Composant graphique JavaFX/Phase 4 - Les tableaux (TableView)/src/main/java/afpa/clement/org/gui/AjoutController.java
+++ Developper des composants d'interface/Developper une interface de type desktop/Composant graphique JavaFX/Phase 4 - Les tableaux (TableView)/src/main/java/afpa/clement/org/gui/AjoutController.java
@@ -4,6 +4,8 @@ import afpa.clement.org.Client;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import static afpa.clement.org.gui.TabController.listClients;
 
@@ -12,10 +14,24 @@ public class AjoutController {
     public TextField prenomField;
     public TextField villeField;
     public Button btnAjout;
+    public Button btnAnnul;
+    public Button btnRetour;
+    public VBox ajout_pane;
 
     public void ajout(ActionEvent actionEvent) {
         if (nomField.getText().length()>0 && prenomField.getText().length()>0){
             listClients.add(new Client(nomField.getText(),prenomField.getText(), villeField.getText()));
         }
+    }
+
+    public void annule(ActionEvent actionEvent) {
+        nomField.setText("");
+        prenomField.setText("");
+        villeField.setText("");
+    }
+
+    public void retour(ActionEvent actionEvent) {
+        Stage stage = (Stage) btnRetour.getScene().getWindow();
+        stage.close();
     }
 }
