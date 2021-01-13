6bca3ae7955f0a306e48708390597f9e94c60f68
BaltoClem <dumasclementjulien@yahoo.fr>
2021-01-13 16:32:38 +0100

    Correction connexion bdd JDBC

--- Developper des composants d'interface/Developper une interface de type desktop/Acces aux donnees - JDBC/Phase 1 - Le mode connecté/Exercices/Exercice 1 - Recherche d'une ligne donnee dans une table/src/main/java/afpa/clement/org/App.java
+++ Developper des composants d'interface/Developper une interface de type desktop/Acces aux donnees - JDBC/Phase 1 - Le mode connecté/Exercices/Exercice 1 - Recherche d'une ligne donnee dans une table/src/main/java/afpa/clement/org/App.java
@@ -7,18 +7,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class App extends Application {
 
-    public static void main(String[] args) {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/afpa/clement/org/gui/RechercheLigne.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
 
         try {
+            String url = "jdbc:mysql://localhost:3306/papyrus";
 
-            String url = "jdbc:mysql://localhost:3306/papyrus?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
             Connection con = DriverManager.getConnection(url, "root", "");
 
             Statement stm = con.createStatement();
@@ -31,24 +34,11 @@ public class App extends Application {
                 String ville = result.getString("vilfou");
                 System.out.format("[%d] %s %s\n", num, nom, ville);
             }
-
             stm.close();
             result.close();
             con.close();
-
-        } catch (Exception e) {
-            System.out.println("Error");
-            System.out.println(e.getMessage());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/afpa/clement/org/gui/Recherche_ligne.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 }
